814a2116771e8e21d4dfb23ab9c0efbdda9a8704
Unique Visuals <theuniquevisuals15@gmail.com>
2026-05-21 19:16:51 +0100

    Fix Vercel deployment: remove custom base path, add SPA rewrites, switch Firebase to env vars

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUsSIFWYQcLboOQ8jubWt0S8MUgebBOnM",
-  authDomain: "theuniquevisuals-49635.firebaseapp.com",
-  projectId: "theuniquevisuals-49635",
-  storageBucket: "theuniquevisuals-49635.firebasestorage.app",
-  messagingSenderId: "123810596212",
-  appId: "1:123810596212:web:b7fd0658b19f172d2bd821",
-  measurementId: "G-GKNYBS9SRF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
